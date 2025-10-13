606172344932a19cfc6ab439d743c84e6fc2acb6
malav patel <patelmalav64@gmail.com>
2025-10-12 23:47:03 -0700

    test cloudflare auto deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="">
+    This will be malvpatel
+</div>
