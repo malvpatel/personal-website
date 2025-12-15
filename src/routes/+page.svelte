4563f53d0763528ee33efb6975fa3df82229d5f2
patel-malav <patelmalav64@gmail.com>
2025-12-14 23:46:30 -0800

    chore: maintaince

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="">
-    This will be malvpatel
-</div>
+<script lang="ts">
+    let loggedIn = $state(false);
+</script>
+
+{#if loggedIn}
+    <div class="">
+        This will be malvpatel123
+    </div>
+{:else}
+    <div>
+        Login
+    </div>
+{/if}
