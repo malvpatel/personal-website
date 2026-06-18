20e1cd863e75c7f6b79ea60e59adc4eea951f966
malav patel <patelmalav64@gmail.com>
2026-06-18 12:13:33 -0700

    work?

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,37 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export interface Position {
+	x: number;
+	y: number;
+}
+
+export interface Rect extends Position {
+	width: number;
+	height: number;
+}
+
+export interface BoxPoints {
+	topLeft: [number, number];
+	topMid: [number, number];
+	topRight: [number, number];
+	centerLeft: [number, number];
+	centerMid: [number, number];
+	centerRight: [number, number];
+	botLeft: [number, number];
+	botMid: [number, number];
+	botRight: [number, number];
+}
+
+/**
+ *
+ * @param input e.g. "example"
+ * @returns "Example" capitalized first letter for input
+ */
+export function upperFirst(input: string): string {
+	if (input.length === 0) return input;
+	return input.charAt(0).toLocaleUpperCase() + input.substring(1);
+}
+
+// export function computeBoxPoints(x: Position['x'], y: Position['y'], width: Rect['width'], height: Rect["height"]): BoxPoints {
+
+// }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<div class="">
-    This will be malvpatel
+<script>
+	import Card from '$lib/components/Card.svelte';
+	import FamilyTree from '../lib/components/FamilyTree.svelte';
+
+	let name = $state('world');
+	let last = $state('');
+</script>
+
+<svelte:head>
+	<title>Malav Patel</title>
+</svelte:head>
+<div class="flex min-h-dvh flex-col">
+	<header>
+		<h1>Malav Patel</h1>
+	</header>
+	<main class="grow p-4">
+		<FamilyTree>
+			<Card col={0} row={0} firstName="prashant" lastName="patel" />
+			<Card col={300} row={0} firstName="parul" lastName="patel" />
+			<Card col={150} row={200} firstName="malav" lastName="patel" />
+			<line x1="150" y1="100" x2="300" y2="100" stroke-width="2" stroke="black"></line>
+			<line x1="225" y1="100" x2="225" y2="200" stroke-width="2" stroke="black"></line>
+		</FamilyTree>
+	</main>
 </div>
